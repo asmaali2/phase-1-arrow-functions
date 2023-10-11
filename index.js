2bb0a80c80554453151f35085552c5dfb2c8e3ad
asmaali2 <asma.maalim7@gmail.com>
2023-10-11 09:41:18 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide= ()=>  2000 / 100;
+ 
+// const divide =2000 divide 1000
+
+// const = square
+
+// const=sum
+
+const square = (x)=> x ** 2; 
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+//   sum(1,2); //=> 3
